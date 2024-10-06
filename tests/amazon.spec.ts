ce72ea550cadbd246833f2004dfbedc153b1fdf1
Vidyabharathi <vidyabharathi.ramachandran@testleaf.com>
2024-10-06 13:10:47 +0530

    Updated code

---
+++ tests/amazon.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.amazon.in/');
+  await page.getByRole('link', { name: 'Today\'s Deals' }).click();
+  await page.getByRole('link', { name: 'POCO Onam Shopping store' }).click();
+  await expect(page.getByLabel('Onam Shopping Store')).toBeVisible();
+  
+});
