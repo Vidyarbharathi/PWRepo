3c9d347c9c58aad91c64622ba32aa04b05322c19
Vidyabharathi <vidyabharathi.ramachandran@testleaf.com>
2024-10-06 13:18:06 +0530

    Updated code

--- tests/amazon.spec.ts
+++ tests/amazon.spec.ts
@@ -5,5 +5,6 @@ test('test', async ({ page }) => {
   await page.getByRole('link', { name: 'Today\'s Deals' }).click();
   await page.getByRole('link', { name: 'POCO Onam Shopping store' }).click();
   await expect(page.getByLabel('Onam Shopping Store')).toBeVisible();
+  console.log("Learning Github actions")
   
 });
